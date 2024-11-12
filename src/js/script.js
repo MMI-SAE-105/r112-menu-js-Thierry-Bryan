78e07fe611af4bf7caa9ec1ccfc76ab6d09148e9
Thierry-Bryan <bryan.thierry@edu.univ-fcomte.fr>
2024-11-12 17:24:17 +0100

    ouverture/fermeture menu

--- src/js/script.js
+++ src/js/script.js
@@ -3,8 +3,12 @@ const toggle = document.querySelector(".menu-btn");
 const nav = document.querySelector("#mainNav");
 
 // Ajoute un écouteur d'événements à l'élément 'toggle' qui écoute les événements 'click'.
+
 toggle.addEventListener("click", () => {
   // Mise à jour des attributs ARIA pour accessibilité
-  nav.ariaHidden = false;
-  toggle.ariaExpanded = true;
+  const isOpen = toggle.ariaExpanded === "true";
+    const isClosed = !isOpen;
+    console.log("isOpen : ", isOpen, "isClosed : ", isClosed);
+  nav.ariaHidden = isOpen;
+  toggle.ariaExpanded = isClosed;
 });
